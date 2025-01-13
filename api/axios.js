f50ddf995827cf7d994a5c927b4ef338791cc19d
RicardoC116 <contrerasr1136@gmail.com>
2025-01-13 13:58:08 -0600

    Produccion

--- api/axios.js
+++ api/axios.js
@@ -2,14 +2,15 @@ import axios from "axios";
 
 const api = axios.create({
   // casa
-  baseURL: "http://192.168.80.235:3000/api",
+  // baseURL: "http://192.168.80.235:3000/api",
 
   // trabajo
   // baseURL: "http://192.168.1.110:3000/api",
 
   // baseURL: "http://192.168.1.101:3000/api",
 
-  // baseURL: "https://cobrobackend2-production.up.railway.app/api",
+  // Produccion
+  baseURL: "https://cobrobackend2-production.up.railway.app/api",
 });
 
 export default api;
